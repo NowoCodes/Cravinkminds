3e773cb3c6cc4a98fc2e21936234856e69c1cf74
Favour Okunowo <43044740+NowoCodes@users.noreply.github.com>
2021-04-04 00:33:35 +0100

    user can now delete books

---
+++ dashboard/deletebook.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+include '../engine/conn.php';
+if (isset($_SESSION['cravinkuname'])) {
+  $cravinkuname = $_SESSION['cravinkuname'];
+} else {
+  header("Location:../login.php");
+}
+	$book_isbn = $_GET['bookisbn'];
+
+	$query = "DELETE FROM books WHERE book_isbn = '$book_isbn'";
+	$result = mysqli_query($conn, $query);
+	if(!$result){
+		echo "delete data unsuccessfully " . mysqli_error($conn);
+		exit;
+	}
+	header("Location: viewbooks.php");
+?>
