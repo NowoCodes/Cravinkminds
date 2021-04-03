012e26180cf8c5557cdcb942021f26bee5e3ff9e
Favour Okunowo <43044740+NowoCodes@users.noreply.github.com>
2021-04-03 20:40:06 +0100

    add function

--- dashboard/vbooks.php
+++ dashboard/vbooks.php
@@ -20,6 +20,15 @@ $email = $gsam['email'];
 $address = $gsam['address'];
 $phone = $gsam['phone'];
 
+function getAll($conn){
+  $query = "SELECT * from books ORDER BY book_isbn DESC";
+  $result = mysqli_query($conn, $query);
+  if(!$result){
+      echo "Can't retrieve data " . mysqli_error($conn);
+      exit;
+  }
+  return $result;
+}
 $result = getAll($conn);
 
 include 'dashboard-nav.php' ?>
