2dedb14bca18dbb84be198853e3813e0f20d9855
Favour Okunowo <43044740+NowoCodes@users.noreply.github.com>
2021-04-03 20:39:57 +0100

    remove function

--- engine/conn.php
+++ engine/conn.php
@@ -10,15 +10,5 @@ if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 
-function getAll($conn){
-    $query = "SELECT * from books ORDER BY book_isbn DESC";
-    $result = mysqli_query($conn, $query);
-    if(!$result){
-        echo "Can't retrieve data " . mysqli_error($conn);
-        exit;
-    }
-    return $result;
-}
- 
 
 ?>
