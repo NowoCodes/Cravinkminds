569f4008219b30f10bfb1a2eb6dd155eca511113
Favour Okunowo <43044740+NowoCodes@users.noreply.github.com>
2021-04-03 20:40:29 +0100

    view books from db on client side

--- books.php
+++ books.php
@@ -1,174 +1,73 @@
-<?php 
+<?php
 $page = 'books';
 $title = "Books - CravinkMinds";
 $description = "Tired of the heart ache of wirtter's block-you want to write but can't get?..........";
 include 'header.php';
 include 'engine/conn.php';
 
+$query = "SELECT * FROM books";
+$result = mysqli_query($conn, $query);
+if (!$result) {
+  echo "Can't retrieve data " . mysqli_error($conn);
+  exit;
+}
 ?>
-
-
-
 
 <h1 class="container-fluid p-4 text-center text-white " style="background: #053a42;">COMMUNITY BOOKS</h1>
 <div class="container mb-5 mt-3">
 
+<p class="lead text-center text-muted">Full Catalogs of Books</p>
+    <?php for($i = 0; $i < mysqli_num_rows($result); $i++): ?>
+      <div class="row">
+        <?php while($query_row = mysqli_fetch_assoc($result)): ?>
+          <div class="col-md-3">
+            <a href="book.php?bookisbn=<?= $query_row['book_isbn']; ?>">
+              <img class="img-responsive img-thumbnail" src="./bootstrap/img/<?=$query_row['book_image']; ?>">
+            </a>
+            <p><?= $query_row['book_title']; ?></p>
+          </div>
+        <?php
+          $count++;
+          if($count >= 4){
+              $count = 0;
+              break;
+            }
+          endwhile; ?> 
+      </div>
+<?php endfor; ?>
 
 
-<div class="card-deck  text-center">
 
-  <div class="card">
-    <img class="card-img-top" src="img/books/book1.jpeg" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title text-center">Learn Creative writing</h5>
-      <p class="card-text">Learn Creative writing: A guide to writing Perfect Drafts. (By Adewale Joel)
-</p>
-      <a href="https://amzn.to/3kZ9tVZ" class="btn-lg hover text-white text-uppercase btn-block btn" target="_blank">Buy on Amazon</a>
-      <a href="https://paystack.com/pay/cravinkmindsbook" class="btn-lg hover text-white text-uppercase btn-block btn" target="_blank">Get Digital copy now</a>
+  <div class="card-deck  text-center">
+
+    <div class="card">
+      <img class="card-img-top" src="img/books/book1.jpeg" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title text-center">Learn Creative writing</h5>
+        <p class="card-text">Learn Creative writing: A guide to writing Perfect Drafts. (By Adewale Joel)
+        </p>
+        <a href="https://amzn.to/3kZ9tVZ" class="btn-lg hover text-white text-uppercase btn-block btn" target="_blank">Buy on Amazon</a>
+        <a href="https://paystack.com/pay/cravinkmindsbook" class="btn-lg hover text-white text-uppercase btn-block btn" target="_blank">Get Digital copy now</a>
+      </div>
     </div>
+
+    <div class="card">
+      <img class="card-img-top" src="img/books/book2.jpeg" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title text-center">Why You have writer’s Block</h5>
+        <p class="card-text">Why You have writer’s Block and how to tackle it. (By Adewale Joel)</p>
+        <a href="https://paystack.com/pay/wyhwb" class="btn-lg hover text-white text-uppercase btn-block btn" target="_blank">Get Digital copy now</a>
+      </div>
+    </div>
+
   </div>
 
-  <div class="card">
-    <img class="card-img-top" src="img/books/book2.jpeg" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title text-center">Why You have writer’s Block</h5>
-      <p class="card-text">Why You have writer’s Block and how to tackle it. (By Adewale Joel)</p>
-      <a href="https://paystack.com/pay/wyhwb" class="btn-lg hover text-white text-uppercase btn-block btn" target="_blank">Get Digital copy now</a>
-    </div>
-  </div>
- 
-</div>
-
-
-</div>
-
-<div class="container-fluid mb-5 p-4 text-center text-white " style="background: #053a42;">
-<h1>EDITOR’S PICKS</h1>
-<br><p>Disclosure: Commissions are gotten from any purchase made through the links below. Cravinkminds will never recommend books that we cannot use or read ourselves. Most of the picks are books that have been personally read and studied by the founder and editor. If you are truly desirous of a topnotch writing career, these books are a must read for you.
-</p>
-</div>
-
-<div class="container mb-5 mt-3">
-
-
-
-<div class="card-deck mb-5 text-center">
-
-  <div class="card">
-    <img class="card-img-top" src="img/books/book3.jpeg" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title text-center">Bird By Bird</h5>
-      <p class="card-text">Bird By Bird (By Anne Lamott)
-</p>
-      <a href="https://amzn.to/3quCtZf" class="btn-lg hover text-white text-uppercase btn-block btn" target="_blank">Buy on Amazon</a>
-      
-    </div>
-  </div>
-
-  <div class="card">
-    <img class="card-img-top" src="img/books/book4.jpeg" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title text-center">On Writing: A memoir of the Craft</h5>
-      <p class="card-text">On Writing: A memoir of the Craft (By Stephen King)</p>
-      <a href="https://amzn.to/3s0jaHu" class="btn-lg hover text-white text-uppercase btn-block btn" target="_blank">Buy on Amazon</a>
-    </div>
-  </div>
-
-  <div class="card">
-    <img class="card-img-top" src="img/books/book5.jpeg" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title text-center">The sense of Style</h5>
-      <p class="card-text">The sense of Style (By Stephen Pinker)</p>
-      <a href="https://amzn.to/3pwdnYD" class="btn-lg hover text-white text-uppercase btn-block btn" target="_blank">Buy on Amazon</a>
-    </div>
-  </div>
- 
-</div>
-
-<div class="card-deck mb-5 text-center">
-
-  <div class="card">
-    <img class="card-img-top" src="img/books/book6.jpeg" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title text-center">On Becoming A Novelist</h5>
-      <p class="card-text">On Becoming A Novelist (By John Gardner)
-</p>
-      <a href="https://amzn.to/3jYs857" class="btn-lg hover text-white text-uppercase btn-block btn" target="_blank">Buy on Amazon</a>
-      
-    </div>
-  </div>
-
-  <div class="card">
-    <img class="card-img-top" src="img/books/book7.jpeg" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title text-center">The sense of Style</h5>
-      <p class="card-text">The sense of Style (By Stephen Pinker)</p>
-      <a href="https://amzn.to/3pwdnYD" class="btn-lg hover text-white text-uppercase btn-block btn" target="_blank">Buy on Amazon</a>
-    </div>
-  </div>
-
-  <div class="card">
-    <img class="card-img-top" src="img/books/book8.jpeg" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title text-center">On writing well</h5>
-      <p class="card-text">On writing well (By William Zinsser)</p>
-      <a href="https://amzn.to/3djepVn" class="btn-lg hover text-white text-uppercase btn-block btn" target="_blank">Buy on Amazon</a>
-    </div>
-  </div>
- 
-</div>
-
-
-
-<div class="card-deck mb-5 text-center">
-
-  <div class="card">
-    <img class="card-img-top" src="img/books/book9.jpeg" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title text-center">Writing Down the Bones</h5>
-      <p class="card-text">Writing Down the Bones (By Natalie Goldberg)
-</p>
-      <a href="https://amzn.to/2NFIyUc" class="btn-lg hover text-white text-uppercase btn-block btn" target="_blank">Buy on Amazon</a>
-      
-    </div>
-  </div>
-
-  <div class="card">
-    <img class="card-img-top" src="img/books/book10.jpeg" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title text-center">Everybody writes (By Ann Handley)</h5>
-      <p class="card-text">NB: Ann Handley’s ‘everbody writes’ is not like the other books here. It’s not really a guide book on becoming a masterly creative writer. But it could easily top the list, no doubt. Ann Handley’s book will, more than anything, help you discover how the writer and his writing skill is indelible even in the twenty-first century.</p>
-      <a href="https://amzn.to/3asIrUI" class="btn-lg hover text-white text-uppercase btn-block btn" target="_blank">Buy on Amazon</a>
-    </div>
-  </div>
-
-  <div class="card">
-    <img class="card-img-top" src="img/books/book11.jpeg" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title text-center">Plot And Structure</h5>
-      <p class="card-text">Plot And Structure (By James Scott Bell)</p>
-      <a href="https://amzn.to/2Nykjaq" class="btn-lg hover text-white text-uppercase btn-block btn" target="_blank">Buy on Amazon</a>
-    </div>
-  </div>
- 
-</div>
 
 </div>
 
+<!-- Page Footer-->
+<?php include 'footer.php'; ?>
 
+</body>
 
-
-
-
-
-
-
-
-
-
-
-   <!-- Page Footer-->
-   <?php include 'footer.php';?>
-  
-  </body>
 </html>
