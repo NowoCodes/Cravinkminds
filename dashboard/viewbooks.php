0c664c2c881741ba0da84dd3cb27295e4a9b2a2a
Favour Okunowo <43044740+NowoCodes@users.noreply.github.com>
2021-04-04 21:17:19 +0100

    view books from dashboard specific to publsher

--- dashboard/viewbooks.php
+++ dashboard/viewbooks.php
@@ -2,24 +2,14 @@
 $title = 'View Books';
 $page = 'viewbooks';
 session_start();
-include '../engine/conn.php';
+// include '../engine/conn.php';
+include '../functions/db_functions.php';
+
 if (isset($_SESSION['cravinkuname'])) {
   $cravinkuname = $_SESSION['cravinkuname'];
 } else {
   header("Location:../login.php");
 }
-
-
-function getAll($conn){
-  $query = "SELECT * from books ORDER BY book_isbn DESC";
-  $result = mysqli_query($conn, $query);
-  if(!$result){
-      echo "Can't retrieve data " . mysqli_error($conn);
-      exit;
-  }
-  return $result;
-}
-$result = getAll($conn);
 
 include 'dashboard-nav.php' ?>
 
@@ -44,7 +34,19 @@ include 'dashboard-nav.php' ?>
         </thead>
 
         <tbody>
-          <?php while ($row = mysqli_fetch_assoc($result)) : ?>
+          <?php 
+          // while ($row = mysqli_fetch_assoc($result)) : 
+          ?>
+          <?php 
+            // $pquery = "SELECT * FROM register";
+            $pquery = "SELECT * FROM register WHERE username='$cravinkuname'";
+            $presult = mysqli_query($conn, $pquery);
+            while($parray = mysqli_fetch_assoc($presult)): 
+            $bookId = $parray['id'];
+            $cquery = "SELECT * FROM books WHERE id = '$bookId' ORDER BY book_isbn DESC";
+            $cresult = mysqli_query($conn, $cquery);
+            while ($row = mysqli_fetch_assoc($cresult)):
+          ?>
             <tr>
               <td><?= $row['book_isbn']; ?></td>
               <td><?= $row['book_title']; ?></td>
@@ -55,6 +57,7 @@ include 'dashboard-nav.php' ?>
               <td><a class="btn btn-sm btn-info" href="editbook.php?bookisbn=<?= $row['book_isbn']; ?>">Edit</a>
               <a class="btn btn-sm btn-danger" href="deletebook.php?bookisbn=<?= $row['book_isbn']; ?>">Delete</a></td>
             </tr>
+          <?php endwhile; ?>
           <?php endwhile; ?>
         </tbody>
       </table>
