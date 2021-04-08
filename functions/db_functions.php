3a3782ea71dc56e3ecff29cf38bd4b669aa4cdee
Favour Okunowo <43044740+NowoCodes@users.noreply.github.com>
2021-04-08 02:34:56 +0100

    Update db_functions.php

--- functions/db_functions.php
+++ functions/db_functions.php
@@ -1,6 +1,14 @@
 <?php
 
 // include '../engine/conn.php';
+function db_connect(){
+  $conn=mysqli_connect('localhost','root','','cravinkm_cravink');
+  if(!$conn){
+    echo "Can't connect database " . mysqli_connect_error($conn);
+    exit;
+  }
+  return $conn;
+}
 
 function getBooks($conn) {
   $pquery = "SELECT * FROM register";
@@ -12,6 +20,28 @@ function getBooks($conn) {
   } else {
     return null;
   }
+}
+
+function getbookprice($isbn){
+  $conn = db_connect();
+  $query = "SELECT book_price FROM books WHERE book_isbn = '$isbn'";
+  $result = mysqli_query($conn, $query);
+  if(!$result){
+    echo "get book price failed! " . mysqli_error($conn);
+    exit;
+  }
+  $row = mysqli_fetch_assoc($result);
+  return $row['book_price'];
+}
+
+function getBookByIsbn($conn, $isbn){
+  $query = "SELECT book_title, book_author, book_price FROM books WHERE book_isbn = '$isbn'";
+  $result = mysqli_query($conn, $query);
+  if(!$result){
+    echo "Can't retrieve data " . mysqli_error($conn);
+    exit;
+  }
+  return $result;
 }
 
 function getAll($conn)
