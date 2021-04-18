8f1a788dc3665c91c76277ba8f111d2154277e04
Favour Okunowo <43044740+NowoCodes@users.noreply.github.com>
2021-04-18 13:43:16 +0100

    add paystack

--- book.php
+++ book.php
@@ -6,8 +6,8 @@ include 'header.php';
 include 'engine/conn.php';
 include 'functions/db_functions.php';
 
-$book_isbn = $_GET['bookisbn'];
-$query = "SELECT * FROM books WHERE book_isbn = '$book_isbn'";
+$id = $_GET['id'];
+$query = "SELECT * FROM books WHERE id = '$id'";
 $result = mysqli_query($conn, $query);
 if (!$result) {
   echo "Can't retrieve data " . mysqli_error($conn);
@@ -22,9 +22,11 @@ if (!$row) {
 ?>
 
 <p class="lead ml-4 mt-4"><a href="books.php">Books</a> > <?= $row['book_title']; ?></p>
-<div class="row">
+<div class="row container">
   <div class="col-md-3 text-center">
+  <a href="./img/books/<?= $row['book_image'] ?>" download="image1">
     <img class="ml-3 img-responsive img-thumbnail" src="./img/books/<?= $row['book_image']; ?>">
+  </a>
   </div>
   <div class="col-md-6">
     <h4>Book Description</h4>
@@ -32,10 +34,6 @@ if (!$row) {
     <h4>Book Details</h4>
     <table class="table">
       <tbody>
-        <tr>
-          <td>ISBN</td>
-          <td><?= $row['book_isbn'] ?></td>
-        </tr>
         <tr>
           <td>Author</td>
           <td><?= $row['book_author'] ?></td>
@@ -70,12 +68,41 @@ if (!$row) {
 
       </tbody>
     </table>
-    <a role="button" target="_blank" href="<?= $row['purchase_link']; ?>" class="btn btn-success mb-4 text-decoration-none">Purchase</a>
+    <?php if (!empty($row['purchase_link'])) : ?>
+    <a role="button" target="_blank" href="<?= $row['purchase_link']; ?>" class="btn btn-success mb-4 text-decoration-none"> Purchase </a>
+    <?php else : ?>
 
-    <!-- <form method="post" action="cart.php">
-      <input type="hidden" name="bookisbn" value="<?php echo $book_isbn; ?>">
-      <input type="submit" value="Purchase / Add to cart" name="cart" class="btn btn-success mb-4">
-    </form> -->
+    <button type="button" class="btn btn-success mb-3" data-toggle="collapse" data-target="#pForm">Purchase</button>
+
+    <div id="pForm" class="collapse">
+      <form id="paymentForm">
+        <div class="form-group">
+          <label for="email">Email address:</label>
+          <input type="email" class="form-control" id="email-address" required>
+        </div>
+        <div class="form-group">
+          <label for="amount">Amount</label>
+          <input type="tel" class="form-control" id="amount" value="<?= $row['book_price']; ?>" required />
+        </div>
+        <div class="row">
+          <div class="col-md-6 form-group">
+            <div class="form-group">
+              <label for="first-name">First Name</label>
+              <input type="text" class="form-control" id="first-name" />
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="form-group">
+              <label for="last-name">Last Name</label>
+              <input type="text" class="form-control" id="last-name" />
+            </div>
+          </div>
+        </div>
+        <button type="submit" class="btn btn-success mb-3" onclick="payWithPaystack()"> Pay </button>
+      </form>
+    </div>
+
+    <?php endif; ?>
   </div>
 </div>
 
@@ -85,6 +112,32 @@ if (!$row) {
 <!-- Page Footer-->
 <?php include 'footer.php'; ?>
 
+<script src="https://js.paystack.co/v1/inline.js"></script>
+<script>
+  const paymentForm = document.getElementById('paymentForm');
+  paymentForm.addEventListener("submit", payWithPaystack, false);
+  function payWithPaystack(e) {
+    e.preventDefault();
+    let handler = PaystackPop.setup({
+      key: 'pk_test_21817dec44e03c6736666c8196ecda193d8c9f9e', // Replace with your public key
+      email: document.getElementById("email-address").value,
+      amount: document.getElementById("amount").value * 100,
+      // ref: '' + Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
+      // label: "Optional string that replaces customer email"
+      onClose: function() {
+        alert('Transaction Cancelled.');
+      },
+      callback: function(response) {
+        let message = 'Payment complete! Reference: ' + response.reference;
+        alert(message);
+      }
+
+    });
+
+    handler.openIframe();
+
+  }
+</script>
 </body>
 
 </html>
