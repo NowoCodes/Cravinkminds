18b32f3d6f284f37abf93b8432a80f607a7e70f8
Favour Okunowo <43044740+NowoCodes@users.noreply.github.com>
2021-04-27 19:28:21 +0100

    add error & success alert styling

--- engine/conn.php
+++ engine/conn.php
@@ -13,4 +13,22 @@ if($link === false){
 
 include '../functions/db_functions.php'; 
 
+session_start();
+
+if (isset($_SESSION['success'])) {
+    echo '<div class="container alert alert-success alert-dismissible fade show">';
+    echo '<button type="button" class="close" data-dismiss="alert">&times;</button>';
+    echo '<li class="text-success list-unstyled">'.$_SESSION['success'].'</li>';
+    echo '</div>';
+    unset($_SESSION['success']);
+}
+
+if (isset($_SESSION['error'])) {
+    echo '<div class="container alert alert-danger alert-dismissible fade show">';
+    echo '<button type="button" class="close" data-dismiss="alert">&times;</button>';
+    echo '<li class="text-danger list-unstyled">'.$_SESSION['error'].'</li>';
+    echo '</div>';
+    unset($_SESSION['error']);
+}
+
 ?>
