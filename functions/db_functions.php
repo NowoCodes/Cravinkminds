3947f946a8d800d1a865f694f88b3565a11b05b3
Favour Okunowo <43044740+NowoCodes@users.noreply.github.com>
2021-04-28 15:13:54 +0100

    convert datetime to elapsed string

--- functions/db_functions.php
+++ functions/db_functions.php
@@ -76,3 +76,22 @@ function display_errors($errors){
   $display .= '</ul>';
   return $display;
 }
+
+// Below function will convert datetime to time elapsed string.
+function time_elapsed_string($datetime, $full = false) {
+  $now = new DateTime;
+  $ago = new DateTime($datetime);
+  $diff = $now->diff($ago);
+  $diff->w = floor($diff->d / 7);
+  $diff->d -= $diff->w * 7;
+  $string = array('y' => 'year', 'm' => 'month', 'w' => 'week', 'd' => 'day', 'h' => 'hour', 'i' => 'minute', 's' => 'second');
+  foreach ($string as $k => &$v) {
+      if ($diff->$k) {
+          $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
+      } else {
+          unset($string[$k]);
+      }
+  }
+  if (!$full) $string = array_slice($string, 0, 1);
+  return $string ? implode(', ', $string) . ' ago' : 'just now';
+}
