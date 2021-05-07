55e2086e0eb97bbb736ab0206f6d1d277d2a3ebc
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-05-07 11:36:30 +0100

    show rating on each book

--- books.php
+++ books.php
@@ -14,17 +14,24 @@ if (!$result) {
 ?>
 
 <h1 class="container-fluid p-4 text-center text-white " style="background: #053a42;">COMMUNITY BOOKS</h1>
-<div class="container mb-5 mt-3 text-center">
+<div class="container mb-5 mt-3 text-justify">
 
   <?php for ($i = 0; $i < mysqli_num_rows($result); $i++) : ?>
     <div class="row">
-      <?php while ($query_row = mysqli_fetch_assoc($result)) : ?>
+      <?php
+      while ($query_row = mysqli_fetch_assoc($result)) :
+        $id = $query_row['id'];
+        $averagequery = "SELECT AVG(rating) AS overall_rating, COUNT(*) AS total_reviews FROM reviews WHERE book_id = '$id'";
+        $averageresult = mysqli_query($conn, $averagequery);
+        $average = mysqli_fetch_assoc($averageresult);
+      ?>
         <div class="col-md-3">
           <a href="book.php?id=<?= $query_row['id']; ?>">
             <img class="img-responsive img-thumbnail" style="height: 300px; width:300px;" src="./img/books/<?= $query_row['book_image']; ?>">
           </a>
           <h5 class="mt-1"><?= $query_row['book_title']; ?></h5>
           <h6 class="font-weight-bold text-secondary"><?= $query_row['book_author'] ?></h6>
+          <span class="mb-1 font-size-16" style="color: teal;"><?= str_repeat('&#9733;', round($average['overall_rating'])) ?></span>
           <h6 class="font-weight-light">N<?= $query_row['book_price'] ?></h6>
         </div>
       <?php
