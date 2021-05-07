f8fcbff27ae359e1510205acc4c26efef98a5ee8
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-05-07 14:24:17 +0100

    fix grid and show currency

--- books.php
+++ books.php
@@ -16,8 +16,8 @@ if (!$result) {
 <h1 class="container-fluid p-4 text-center text-white " style="background: #053a42;">COMMUNITY BOOKS</h1>
 <div class="container mb-5 mt-3 text-justify">
 
-  <?php for ($i = 0; $i < mysqli_num_rows($result); $i++) : ?>
-    <div class="row">
+  <div class="row">
+    <?php for ($i = 0; $i < mysqli_num_rows($result); $i++) : ?>
       <?php
       while ($query_row = mysqli_fetch_assoc($result)) :
         $id = $query_row['id'];
@@ -25,14 +25,14 @@ if (!$result) {
         $averageresult = mysqli_query($conn, $averagequery);
         $average = mysqli_fetch_assoc($averageresult);
       ?>
-        <div class="col-md-3 col-sm-4 col-6">
+        <div class="col-lg-3 col-md-4 col-sm-6 col-6">
           <a href="book.php?id=<?= $query_row['id']; ?>">
             <img class="img-responsive img-thumbnail" style="height: 300px; width:300px;" src="./img/books/<?= $query_row['book_image']; ?>">
           </a>
           <h5 class="mt-1"><?= $query_row['book_title']; ?></h5>
           <h6 class="font-weight-bold text-secondary"><?= $query_row['book_author'] ?></h6>
           <p class="mt-n2 font-size-16" style="color: teal;"><?= str_repeat('&#9733;', round($average['overall_rating'])) ?></p>
-          <h6 class="mt-n3 font-weight-light">&#8358;<?= $query_row['book_price'] ?></h6>
+          <h6 class="mt-n3 font-weight-light"><?= $query_row['book_price'] ?></h6>
         </div>
       <?php
         $count++;
@@ -41,8 +41,8 @@ if (!$result) {
           break;
         }
       endwhile; ?>
-    </div>
-  <?php endfor; ?>
+    <?php endfor; ?>
+  </div>
 </div>
 
 <!-- Page Footer-->
